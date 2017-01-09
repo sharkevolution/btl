24d24356b038f88a49c082a3f4a8cee94bff8a50
Sitala <striboga@yandex.ru>
2017-01-09 21:48:47 +0200

    first

--- web_shark/mysite/lib/calc.js
+++ web_shark/mysite/lib/calc.js
@@ -8,10 +8,15 @@ window.onload = function() {
     period,
     operator,
     current_figure,
-    current_amount;
+    current_amount,
+    fruit;
 
-    current_figure = 0;
-    current_amount = 0;
+    var current_figure = 0;
+    var current_amount = 0;
+
+    var fruit_rm;
+    var fruit = {};
+    var idx_fruit;
 
     document.getElementById('size').value = 0;
     document.getElementById('figure').disabled = false;
@@ -26,6 +31,73 @@ window.onload = function() {
 
     var elem = document.querySelectorAll(".num");
     var len = elem.length;
+
+    function fruitnew(idx_fruit){
+
+      /* Добавление элементов html от количества данных в массиве fruit */
+      var fld = document.createElement('fieldset');
+      fld.className = 'fld' + String(idx_fruit);
+      // listfruit.appendChild(fld);
+      listfruit.insertBefore(fld, listfruit.children[0]);
+
+      var input_load = document.createElement('input');
+      input_load.className = 'inp operator';
+      input_load.type = 'checkbox';
+      input_load.value="Start";
+
+      fld.appendChild(input_load);
+
+      var label = document.createElement('label');
+      label.for = 'raz';
+
+      var str = String(fruit[idx_fruit].f) + ', ' + String(fruit[idx_fruit].c)
+      label.innerHTML = str;
+
+      fld.appendChild(label);
+    }
+
+    function fruitremove(){
+      /* Удаление данных из массива и списка на странице */
+
+      remcount = 0;
+      var cheks = document.getElementsByClassName('operator');
+      for (i = 0; i < cheks.length; i++){
+        if (cheks[i].checked == true){
+          remcount += 1;
+        }
+      }
+
+      if (remcount != 0){
+        for (s = 0; s < remcount + 1; s++){
+            var cheks = document.getElementsByClassName('operator');
+            for (p = 0; p < cheks.length; p++){
+                if (cheks[p].checked == true){
+                  fruit_rm = p;
+                  break;
+                }
+              }
+
+              invert = cheks.length - fruit_rm  - 1
+              // alert('удаляется в массиве: ' + fruit[invert].f)
+              delete fruit[invert];
+
+              rm = document.getElementById('listfruit').children[p];
+              rm.remove();
+
+              /* Пересоздание массива */
+              var new_fruit = {};
+              m = 0;
+              for(var h in fruit) {
+                new_fruit[m] = fruit[h];
+                // alert(new_fruit[m].f);
+                m += 1;
+              }
+              fruit = new_fruit;
+              idx_fruit -= 1;
+              // alert('idx' + idx_fruit);
+        }
+      }
+    }
 
     for(var i = 0; i < len; i++ ) {
 
@@ -69,6 +141,18 @@ window.onload = function() {
         var str = String(ch2)
         document.getElementById('crf').childNodes.item(0).nodeValue = 'Figure: ' + ch2;
         current_figure = ch2;
+
+        if (current_figure > 0 && current_amount > 0){
+
+          var size = Object.keys(fruit).length
+          if (size == 0){
+            idx_fruit = 0
+          }
+          fruit[idx_fruit] = {'f': current_figure, 'c': current_amount};
+          fruitnew(idx_fruit);
+          idx_fruit += 1;
+        }
+
       }
 
     },false);
@@ -85,6 +169,18 @@ window.onload = function() {
         var str = String(ch2)
         document.getElementById('amf').childNodes.item(0).nodeValue = 'Amount: ' + ch2;
         current_amount = ch2;
+
+        if (current_figure > 0 && current_amount > 0){
+
+          var size = Object.keys(fruit).length
+          if (size == 0){
+            idx_fruit = 0
+          }
+          fruit[idx_fruit] = {'f': current_figure, 'c': current_amount};
+          fruitnew(idx_fruit);
+          idx_fruit += 1;
+        }
+
       }
 
     },false);
@@ -101,24 +197,26 @@ window.onload = function() {
         current_figure = 0;
         current_amount = 0;
 
+        fruitremove();
+
 
     },false);
 
-    // var elem1 = document.querySelectorAll(".operator");
-    // var len1 = elem1.length;
-    //
-    // for(var i = 0; i < len1; i++ ) {
-    //
-    //     elem1[i].addEventListener("click",function() {
-    //     operator = this.value;
-    //
-    //      if(screen.innerHTML === "") {
-    //         screen.innerHTML = screen.innerHTML.concat("");
-    //
-    //     }
-    //     else if(output) {
-    //         screen.innerHTML = output.concat(operator);
-    //     }
-    //   },false);
-    // }
+    var elem1 = document.querySelectorAll(".operator");
+    var len1 = elem1.length;
+
+    for(var i = 0; i < len1; i++ ) {
+
+        elem1[i].addEventListener("click",function() {
+        operator = this.value;
+
+         if(screen.innerHTML === "") {
+            screen.innerHTML = screen.innerHTML.concat("");
+
+        }
+        else if(output) {
+            screen.innerHTML = output.concat(operator);
+        }
+      },false);
+    }
 }
